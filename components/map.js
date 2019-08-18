c93184c1cc424b008695643b0e618a80d85299c8
ilumii <listeven073@gmail.com>
2019-08-18 01:20:22 -0400

    centered on manhattan

--- components/map.js
+++ components/map.js
@@ -2,18 +2,21 @@ import React from 'react'
 import { TouchableOpacity, Text, Dimensions, StyleSheet, View } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import MapView from 'react-native-maps';
-
+import { regionFrom } from './getRegion'
 
 export default class App extends React.Component {
+
    render() {
+      let data = regionFrom(40.7831, -73.9712, 10000);
+      console.log(data);
       return (
          <View style={styles.container}>
             <MapView style={styles.map}
                initialRegion={{
-                  latitude: 37.78825,
-                  longitude: -122.4324,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
+                  latitude: 40.7549,
+                  longitude: -73.9840,
+                  latitudeDelta: 0,
+                  longitudeDelta: 0.08983111749910169,
                }}
             >
             </MapView>
@@ -23,20 +26,20 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-   container:{
+   container: {
       position: 'absolute',
-      top:0,
-      left:0,
-      bottom:0,
-      right:0,
-      justifyContent:'flex-end',
-      alignItems:'center'
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      justifyContent: 'flex-end',
+      alignItems: 'center'
    },
-   map:{
-      position:'absolute',
-      top:0,
-      left:0,
-      bottom:0,
-      right:0
+   map: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0
    }
 });
