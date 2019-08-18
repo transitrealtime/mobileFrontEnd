88f518b7c8c5801b85b5c0032750e8c503fb088c
azheng4119 <azheng4119@bths.edu>
2019-08-18 01:08:48 -0400

    white background for cards

--- components/route.js
+++ components/route.js
@@ -8,14 +8,14 @@ import SingleTrainStation from '../pages/singleTrainStation';
 import map from './map.js';
 
 const Routes = () => (
-   <Router {...sceneConfig}>
-      <ScrollView>
-         <Scene key="landing" component={Landing} title="Home" initial={true} />
-         <Scene key="train" component={Train} title="Trains" />
-         <Scene key="singleTrain" component={Single} title="Single" />
-         <Scene key="singleTrainStation" component={SingleTrainStation} title="Station" />
-         <Scene key="map" component={map} title="Station" />
-      </ScrollView>
-   </Router>
+      <Router cardStyle = {{backgroundColor : 'white'}}>
+         <ScrollView>
+            <Scene key="landing" component={Landing} title="Home" initial={true} />
+            <Scene key="train" component={Train} title="Trains" />
+            <Scene key="singleTrain" component={Single} title="Single" />
+            <Scene key="singleTrainStation" component={SingleTrainStation} title="Station" />
+            <Scene key="map" component={map} title="Station" />
+         </ScrollView>
+      </Router>
 )
 export default Routes
