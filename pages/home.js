d9d6df3d3437ad192965468875e85623f99e2a80
enterdoki <chen.tommy925@gmail.com>
2019-08-18 13:12:00 -0400

    react navigation home and map buttons mapped

--- pages/home.js
+++ pages/home.js
@@ -4,15 +4,9 @@ import { Actions } from 'react-native-router-flux';
 import TrainsView from '../components/trainView';
 
 const Home = () => {
-    const goToAbout = () => {
-        Actions.map()
-    }
     return (
         <View style={styles.container}>
             <TrainsView></TrainsView>
-            <TouchableOpacity onPress={goToAbout}>
-            <Text style={{ textAlign: "center" , fontSize: 100}}>Made with love</Text>
-            </TouchableOpacity>
         </View>
     )
 }
