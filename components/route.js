388da84e1af7c363e1f3e9c849ab9783cf8c07b3
azheng4119 <azheng4119@bths.edu>
2019-08-18 00:55:13 -0400

    :/

--- components/route.js
+++ components/route.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import { Router, Scene } from 'react-native-router-flux'
-import { ScrollView } from 'react-native'
+import { View } from 'react-native'
 import Landing from '../pages/home.js'
 import About from './about.js'
 import Train from './trainView'
 import Single from '../pages/singleTrain'
 import SingleTrainStation from '../pages/singleTrainStation';
 
+var sceneConfig = {
+   cardStyle: {
+         backgroundColor: 'white'
+   }
+}
+
 const Routes = () => (
-      <Router>
-         <ScrollView>
-            <Scene key="landing" component={Landing} title="Home" initial={true} />
-            <Scene key="about" component={About} title="About" />
-            <Scene key="train" component={Train} title="Trains" />
-            <Scene key="singleTrain" component={Single} title="Single" />
-            <Scene key="singleTrainStation" component={SingleTrainStation} title="Station" />
-         </ScrollView>
-      </Router>
+   <Router {...sceneConfig}>
+      <View>
+         <Scene key="landing" component={Landing} title="Home" initial={true} />
+         <Scene key="about" component={About} title="About" />
+         <Scene key="train" component={Train} title="Trains" />
+         <Scene key="singleTrain" component={Single} title="Single" />
+         <Scene key="singleTrainStation" component={SingleTrainStation} title="Station" />
+      </View>
+   </Router>
 )
 export default Routes
--- pages/home.js
+++ pages/home.js
@@ -4,9 +4,6 @@ import { Actions } from 'react-native-router-flux';
 import TrainsView from '../components/trainView';
 
 const Home = () => {
-    const goToAbout = () => {
-        Actions.about()
-    }
     return (
         <View style={styles.container}>
             <TrainsView></TrainsView>
