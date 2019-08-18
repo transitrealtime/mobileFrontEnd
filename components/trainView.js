2f3ecb2ad8737c32d7af4c8d1ac81ffc059bff9c
azheng4119 <azheng4119@bths.edu>
2019-08-18 14:17:22 -0400

    changed structure of train view

--- components/trainView.js
+++ components/trainView.js
@@ -3,10 +3,10 @@ import { StyleSheet, Text, View, TouchableOpacity, ScrollView, PixelRatio } from
 import { Actions } from 'react-native-router-flux'
 
 
-let FONT_BACK_LABEL = 60;
+let FONT_BACK_LABEL = 40;
 
 if (PixelRatio.get() <= 2) {
-	FONT_BACK_LABEL = 40;
+    FONT_BACK_LABEL = 30;
 }
 
 const trainsView = () => {
@@ -35,34 +35,55 @@ const trainsView = () => {
         "W": "#FCCC0A",
         "Z": "#996633"
     }
-    
-    const goToSingleTrain = (train) => {
-        Actions.singleTrain({trains: `${train}`, title:`${train} Train`});
-    }
 
-    let train = ["1", "2", "3", "4", "5", "6", "7", "A", "C", "E", "B", "D", "F", "M", "N", "Q", "R", "W", "J", "Z", "L", "S", "G"];
-    let display = train.map(item => {
+    const goToSingleTrain = (train) => {
+        Actions.singleTrain({ trains: `${train}`, title: `${train} Train` });
+    }
+    let redTrains = ["1", "2", "3"];
+    let greenTrains = ["4", "5", "6"];
+    let blueTrains = ["A", "C", "E"];
+    let orangeTrains = ["B", "D", "F", "M"];
+    let yellowTrains = ["N", "Q", "R", "W"];
+    let grayTrains = ["L","S"];
+    let brownTrains = ["J", "Z"];
+    let purpleTrains = ["7","G"];
+    //let lightGreenTrains = ["G"];
+    let allTrains = [redTrains, greenTrains, blueTrains, orangeTrains, yellowTrains, grayTrains, brownTrains, purpleTrains];
+    //let train = ["1", "2", "3", "4", "5", "6", "A", "C", "E", "B", "D", "F", "M", "N", "Q", "R", "W", "L" ,"J","Z", "S", "G","7"];
+    let x = 0;
+    let display = allTrains.map(trains => {
+        let innerDisplay = trains.map(item => {
+            return (
+                <TouchableOpacity onPress={() => goToSingleTrain(item)} style={[style.circles, { borderColor: trainColors[item] }]} key={item}><Text style={{ color: trainColors[item], fontSize: FONT_BACK_LABEL }}>{item}</Text></TouchableOpacity>
+            )
+        })
         return (
-            <TouchableOpacity onPress={() => goToSingleTrain(item)} style={[style.circles, { borderColor: trainColors[item] }]} key={item}><Text style={{ color: trainColors[item], fontSize: FONT_BACK_LABEL }}>{item}</Text></TouchableOpacity>
+            <View key = {x++} style = {{flexDirection :'row'}}>
+                {innerDisplay}
+            </View>
         )
     })
+    // let display = train.map(item => {
+    //     return (
+    //         <TouchableOpacity onPress={() => goToSingleTrain(item)} style={[style.circles, { borderColor: trainColors[item] }]} key={item}><Text style={{ color: trainColors[item], fontSize: FONT_BACK_LABEL }}>{item}</Text></TouchableOpacity>
+    //     )
+    // })
     return (
-        <ScrollView>
-            <View style={style.container}>
-                {display}
-            </View>
-        </ScrollView>
+        //<ScrollView>
+        <View style={style.container}>
+            {display}
+        </View>
+        //</ScrollView>
     )
 }
 
 let style = StyleSheet.create({
     container: {
         marginTop: 10,
-        display : 'flex',
-        flexDirection : 'row',
+        display: 'flex',
+        flexDirection: 'column',
         flexWrap: 'wrap',
-        alignItems: "center",
-        alignContent : 'center',
+        // alignContent : 'center',
         justifyContent: "center",
     },
     circles: {
--- pages/home.js
+++ pages/home.js
@@ -13,8 +13,6 @@ const Home = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        // flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
     },
