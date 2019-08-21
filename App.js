44ca5a78f148df8af0b2a4c209d964cbf6e56946
enterdoki <chen.tommy925@gmail.com>
2019-08-20 23:37:03 -0400

    increased clickable radius of hearts and active footer tabs based on screen

--- App.js
+++ App.js
@@ -3,35 +3,42 @@ import Routes from './components/route';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
-export default App = () => {
-  const goHome = () => {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activePage: 1
+    };
+  }
+  goHome = () => {
     Actions.landing()
   }
-  const goMap = () => {
+  goMap = () => {
     Actions.allMaps()
   }
 
-  const goTwitter = () => {
+  goTwitter = () => {
     Actions.twitterFeed()
   }
- 
-  return (
-    <Container style = {{display : 'flex',flexDirection : 'column', justifyContent: 'center',alignContent:'center'}}>
-      <Routes></Routes>
-      <Footer>
-        <FooterTab>
-          <Button onPress={goHome}>
-            <Icon name="subway" />
-          </Button>
-          <Button onPress={goMap}>
-            <Icon active name="map" />
-          </Button>
-          <Button onPress={goTwitter}>
-            <Icon name="logo-twitter" />
-          </Button>
-        </FooterTab>
-      </Footer>
-    </Container>
+  render() {
+    return (
+      <Container style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center' }}>
+        <Routes></Routes>
+        <Footer>
+          <FooterTab>
+            <Button active={this.state.activePage === 1} onPress={async()=> {await this.setState({activePage:1});this.goHome()}}>
+              <Icon name="subway" />
+            </Button>
+            <Button active={this.state.activePage === 2} onPress={async()=> {await this.setState({activePage:2});this.goMap()}}>
+              <Icon active name="map" />
+            </Button>
+            <Button active={this.state.activePage === 3} onPress={async()=> {await this.setState({activePage:3});this.goTwitter()}}>
+              <Icon name="logo-twitter" />
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  };
 
-  );
 }
